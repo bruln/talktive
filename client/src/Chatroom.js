2153e1757db80e2e00bcd7b31a0f4ab873316df4
bru <brunodlenza@gmail.com>
2021-06-30 20:09:20 -0500

    addtional components

--- client/src/Chatroom.js
+++ client/src/Chatroom.js
@@ -1,32 +1,14 @@
 import React from 'react';
+import MessageBox from './MessageBox';
+import Conversation from './Conversation';
 import { useState } from 'react';
 
 const Chatroom = () => {
-  const [message, setMessage] = useState('');
-
-  function sendMessage(e) {
-    e.preventDefault();
-    if (message) {
-      console.log(message);
-      setMessage('');
-      e.target.reset();
-    } else {
-      // prompt user
-    }
-  }
-
   return (
-    <div>
-      <form onSubmit={sendMessage}>
-        <input
-          type='text'
-          id='message'
-          autoComplete='off'
-          onChange={(e) => setMessage(e.target.value)}
-        ></input>
-        <button type='submit'>send</button>
-      </form>
-    </div>
+    <section>
+      <Conversation />
+      <MessageBox />
+    </section>
   );
 };
 
--- client/src/MessageBox.js
+++ client/src/MessageBox.js
@@ -1,2 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
+
+const MessageBox = () => {
+  const [message, setMessage] = useState('');
+
+  function sendMessage(e) {
+    e.preventDefault();
+    if (message) {
+      console.log(message);
+      setMessage('');
+      e.target.reset();
+    } else {
+      // prompt user
+    }
+  }
+  return (
+    <section>
+      <form onSubmit={sendMessage}>
+        <input
+          type='text'
+          id='message'
+          autoComplete='off'
+          onChange={(e) => setMessage(e.target.value)}
+        ></input>
+        <button type='submit'>send</button>
+      </form>
+    </section>
+  );
+};
+
+export default MessageBox;
